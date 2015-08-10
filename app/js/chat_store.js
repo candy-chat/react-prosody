bb38aaa635418eb61c7e1f46f844ccd1da3255ad
Rafael Garcia <rafbgarcia@gmail.com>
2015-08-10 00:43:07 -0300

    Add Candy's own files.

--- app/js/chat_store.js
+++ app/js/chat_store.js
@@ -1,3 +1,20 @@
+Candy.init('/http-bind/', { // uncomment & comment next line if you'd like to use BOSH
+// Candy.init('ws://localhost:5280/xmpp-websocket/', {
+  core: {
+    // only set this to true if developing / debugging errors
+    debug: true,
+    // autojoin is a *required* parameter if you don't have a plugin (e.g. roomPanel) for it
+    //   true
+    //     -> fetch info from server (NOTE: does only work with openfire server)
+    //   ['test@conference.example.com']
+    //     -> array of rooms to join after connecting
+    autojoin: ['test@conference.localhost']
+  },
+  view: { assets: 'res/' }
+});
+
+Candy.Core.connect('localhost');
+
 (function(Reflux, ChatActions, global) {
   'use strict';
 
@@ -6,8 +23,6 @@
 
     onNewMessage: function(message) {
       var messages = this.messages.concat([{body: message, mine: true}]);
-
-      send(message);
 
       this.updateList(messages);
     },
